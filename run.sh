189231c676fc4096d46c3ce29087f6ad69e3b639
xjh19971 <414581775@qq.com>
2023-05-08 14:43:06 -0400

    run.sh

--- run.sh
+++ run.sh
@@ -10,4 +10,6 @@ conda activate VS
 # python video_stab.py --video_name 4.avi --save --estModel affine --smooth_win_len 250 --smooth_order 3 --max_frame 250 --debug --feat_ext GFTT
 # python video_stab.py --video_name 4.avi --save --estModel homography --smooth_win_len 250 --smooth_order 3 --max_frame 250 --debug --feat_ext GFTT
 # python video_stab.py --video_name 1.mp4 --dataset UAV --save --estModel affine --smooth_win_len 250 --smooth_order 3 --max_frame 1500 --video_offset 900 --debug --feat_ext GFTT
-# python video_stab.py --video_name 1.mp4 --dataset UAV --save --estModel homography --smooth_win_len 250 --smooth_order 3 --max_frame 1500 --video_offset 900 --debug --feat_ext GFTT
+# python video_stab.py --video_name 1.mp4 --dataset UAV --save --estModel homography --smooth_win_len 250 --smooth_order 3 --max_frame 1500 --video_offset 900 --feat_ext GFTT
+# python video_stab.py --video_name 1.mp4 --dataset UAV1 --save --estModel affine --smooth_win_len 250 --smooth_order 3 --max_frame 900 --video_offset 0 --debug --feat_ext GFTT
+# python video_stab.py --video_name 1.mp4 --dataset UAV1 --save --estModel homography --smooth_win_len 250 --smooth_order 3 --max_frame 900 --video_offset 0 --debug --feat_ext GFTT
