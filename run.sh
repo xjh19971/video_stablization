da4cffd70e2807e970e874d9e9201661d179f5cb
xjh19971 <jx1190@nyu.edu>
2023-05-07 23:14:22 -0400

    finish script

--- run.sh
+++ run.sh
@@ -7,5 +7,7 @@ conda activate VS
 # python video_stab.py --video_name 1.avi --save --estModel homography --smooth_win_len 250 --smooth_order 3 --max_frame 250 --debug --feat_ext GFTT
 # python video_stab.py --video_name 3.avi --save --estModel affine --smooth_win_len 250 --smooth_order 3 --max_frame 250 --debug --feat_ext GFTT
 # python video_stab.py --video_name 3.avi --save --estModel homography --smooth_win_len 250 --smooth_order 3 --max_frame 250 --debug --feat_ext GFTT
-python video_stab.py --video_name 1.mp4 --dataset UAV --save --estModel affine --smooth_win_len 250 --smooth_order 3 --max_frame 1500 --video_offset 900 --debug --feat_ext GFTT
-python video_stab.py --video_name 1.mp4 --dataset UAV --save --estModel homography --smooth_win_len 250 --smooth_order 3 --max_frame 1500 --video_offset 900 --debug --feat_ext GFTT
+# python video_stab.py --video_name 4.avi --save --estModel affine --smooth_win_len 250 --smooth_order 3 --max_frame 250 --debug --feat_ext GFTT
+# python video_stab.py --video_name 4.avi --save --estModel homography --smooth_win_len 250 --smooth_order 3 --max_frame 250 --debug --feat_ext GFTT
+# python video_stab.py --video_name 1.mp4 --dataset UAV --save --estModel affine --smooth_win_len 250 --smooth_order 3 --max_frame 1500 --video_offset 900 --debug --feat_ext GFTT
+# python video_stab.py --video_name 1.mp4 --dataset UAV --save --estModel homography --smooth_win_len 250 --smooth_order 3 --max_frame 1500 --video_offset 900 --debug --feat_ext GFTT
